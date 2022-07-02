a1e32f0c8f5c1ea5ba6b91ef8162b109b9c89847
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-02 23:32:21 +0600

    components creating

---
+++ src/theme/spacing.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Spacing() {
+  return (
+    <View>
+      <Text>spacing</Text>
+    </View>
+  )
+}
---
+++ src/theme/typography.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function typography() {
+  return (
+    <View>
+      <Text>typography</Text>
+    </View>
+  )
+}
