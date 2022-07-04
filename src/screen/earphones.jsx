ac02959518544aad89eb2dfe5f5b1d289ff234cc
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-04 23:53:19 +0600

    update

---
+++ src/screen/earphones.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Earphones() {
+  return (
+    <View>
+      <Text>Earphones</Text>
+    </View>
+  )
+}
---
+++ src/screen/headphones.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Headphones() {
+  return (
+    <View>
+      <Text>Headphones</Text>
+    </View>
+  )
+}
