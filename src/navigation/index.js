09686b3cccdd6332354c5331d766e7626b983353
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-05 00:29:18 +0600

    update

---
+++ src/navigation/index.js
@@ -0,0 +1,35 @@
+import { NavigationContainer, DefaultTheme, Tab } from "@react-navigation/native";
+import { colors } from './../theme/colors';
+import Home from './../screen/home';
+
+
+
+const THEME = {
+    ...DefaultTheme,
+
+    colors: {
+        ...DefaultTheme.colors,
+        background: 'white'
+    }
+}
+
+const Tab = createBottomTabNavigator();
+
+const myStack = createStackNavigator();
+
+function homeStackScreen (){
+    return(
+        <Tab.Navigation>
+            <Tab.Screen name= "Home" component = {Home}  />
+       </Tab.Navigation>
+    )
+
+}
+
+export default function Navigation(){
+    <NavigationContainer theme = {THEME}>
+        
+            <Tab.Screen name= "Setting" component = {Home}  />
+        
+    </NavigationContainer>
+}
