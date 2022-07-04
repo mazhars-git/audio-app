6dce449963e89225f89865089fc45b2756f0a237
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-04 23:55:43 +0600

    update

--- src/screen/speakers.jsx
+++ src/screen/speakers.jsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function CartDetails() {
+export default function Speakers() {
   return (
     <View>
-      <Text>CartDetails</Text>
+      <Text>Speakers</Text>
     </View>
   )
 }
