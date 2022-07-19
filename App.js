8023653db2191bf55ed1bd9151ead5edbc5b721e
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-07-19 23:42:41 +0600

    bottom nav stack

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    'Manrope-Bold': require('./assets/fonts/Manrope-Bold.ttf'),
+    'Manrope-Medium': require('./assets/fonts/Manrope-Medium.ttf'),
+    'Manrope-Regular': require('./assets/fonts/Manrope-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  else{
+    
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+ 
 }
 
 const styles = StyleSheet.create({
